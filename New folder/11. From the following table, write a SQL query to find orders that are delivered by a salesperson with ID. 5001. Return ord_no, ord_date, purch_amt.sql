a1e1027013e2ef6b7b1f80294b44f4dd7cb2c92b
Kaif Tahir <110182266+Md-Kaif-Tahir@users.noreply.github.com>
2022-08-13 23:36:33 +0530

    Delete New folder directory

--- New folder/11. From the following table, write a SQL query to find orders that are delivered by a salesperson with ID. 5001. Return ord_no, ord_date, purch_amt.sql
+++
@@ -1,6 +0,0 @@
-SHOW DATABASES;
-USE practice;
-SHOW TABLES;
-SELECT*FROM orders;
-
-SELECT ord_no,ord_date,purch_amt FROM orders WHERE salesman_id = 5001
--- New folder/9. From the following table, write a SQL query to locate salespeople who live in the city of 'Paris'. Return salesperson's name, city..sql
+++
@@ -1,5 +0,0 @@
-SHOW databases;
-USE practice;
-SHOW TABLES;
-SELECT name,city FROM salesman WHERE city = 'Paris';
-SELECT*FROM salesman
